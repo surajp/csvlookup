888fa45756e641d82deee84c5d6d12e1f9af45a8
Suraj <85.suraj@gmail.com>
2016-02-05 12:25:36 -0500

    Added gitignore and changed special char sequence to replace commans

--- processcsv.sh
+++ processcsv.sh
@@ -1,6 +1,6 @@
 < $1 tr -d -c "[:print:]^\n" > tempf.csv
 awk -F '"' -v RS="^^^^^^^" -v OFS='"' '{for(i=2;i<=NF;i+=2){gsub("\n","",$i)}} 1' tempf.csv > tempf1.csv
-awk -F '"' -v OFS='"' '{for(i=2;i<=NF;i+=2){gsub(",","|||",$i)}} 1' tempf1.csv > tempf2.csv #TODO - Replace special char sequence with comma once processing is complete
+awk -F '"' -v OFS='"' '{for(i=2;i<=NF;i+=2){gsub(",","@@@@@",$i)}} 1' tempf1.csv > tempf2.csv #TODO - Replace special char sequence with comma once processing is complete
 awk 'NF' tempf2.csv > tempf3.csv
 awk -F ',' 'BEGIN{min=1000;max=0} {if(NF<min){min=NF};if(NF>max){max=NF}} END{print min" "max}' tempf3.csv
 mv tempf3.csv $1_op
